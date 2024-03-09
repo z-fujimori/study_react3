2d1eb946afa45dee3a26a821a7c4bdbac2cdffd4
z-fujimori <fuji.tai.29@i.softbank.jp>
2024-03-10 00:09:23 +0900

    move button 3_9

--- src/components/MoveButton/MoveButton.js
+++ src/components/MoveButton/MoveButton.js
@@ -1,23 +1,24 @@
+import { useState } from 'react';
 import React from 'react'
 import './MoveButton.css'
 import sound from './pi.mp3'
 
 const MoveButton = () => {
-
+    const [count, setCount] = useState(0)
     const audio = new Audio(sound);
     audio.volume = 0.3;
     let top = "100px";
     let left = "10%";
-    var random_top = 0;
-    var random_left = 0;
 
     const move = () => {
         audio.play().then(() => {
             console.log("Audio started!")
         })
-        random_top = Math.floor(Math.random()*101)
-        random_left = Math.floor(Math.random()*300)
-        
+        left = Math.floor(Math.random()*95) + "%"
+        top = Math.floor(Math.random()*700) + "px"
+        document.documentElement.style.setProperty('--top', top)
+        document.documentElement.style.setProperty('--left', left)
+        setCount(count + 1)
     }
 
   return (
